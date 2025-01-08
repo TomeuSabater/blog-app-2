fe5ca83508e49960d85d240831d958088985d151
TomeuSabater <bmesabater@gmail.com>
2025-01-09 00:48:32 +0100

    Primeras Rutas

--- routes/web.php
+++ routes/web.php
@@ -7,8 +7,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
-
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
@@ -18,6 +16,32 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+//////  Routes simples
+
+Route::get('/hola',function() {
+    return '<h1>Hola Mundo Cruel</h1>';
+}); 
+
+Route::get('/index.html',function() {
+
+    $html_code = "<!DOCTYPE html>
+                    <html>
+                    <head>
+                        <title>Laravel</title>
+                    </head>
+                    <body>
+                        <h1>I love Laravel</h1>
+                    </body>
+                    </html>"; 
+
+    return $html_code;
+}); 
+
+
+
+
+
 
 
 require __DIR__.'/auth.php';
